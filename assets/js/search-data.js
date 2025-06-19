8fe1916a4e53c776d33731a5f16a6d703d4ffcf9
stew31415 <alexstewart314@gmail.com>
2025-06-19 03:39:24 +0000

    Deploying to gh-pages from @ stew31415/stew31415.github.io@79775e1a671e1e741c4a80b5143b8d38d66f1c53 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -387,8 +387,8 @@ ninja.data = [{
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
           description: "",
-          section: "News",},{id: "projects-project-1",
-          title: 'project 1',
+          section: "News",},{id: "projects-photography",
+          title: 'Photography',
           description: "with background image",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project/";
